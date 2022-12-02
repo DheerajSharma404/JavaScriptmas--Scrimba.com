45f42ea8489a6b9e2f36c52cf25c2357dc00a327
Dheeraj Sharma <at60sharwan@gmail.com>
2022-12-02 13:57:43 +0530

    JavaScriptma Day_1 Solution--Created the transformData function.

---
+++ DAY_2/DAY_2.js
@@ -0,0 +1,35 @@
+// ********JavaScriptmas_DAY-2_Challenge**********
+/* Totally Private Data Farm 
+
+Good news, renown advertising firm Evil Corp. wants to purchase our 
+private user data! 
+
+We'd never do this in real life of course, but just for practice 
+let's pretend we're unethical web hackers and transform the data 
+in the way Evil Corp. has requested. They're quite particular and
+just want an array of users with a fullname and human readable
+birthday.   
+
+Write a function that maps through the current data and returns
+a new an array of objects with only two properties: 
+fullName and birthday. Each result in your 
+array should look like this when you're done: 
+
+{
+   fullName: "Levent Busser",  
+   birthday: "Fri Aug 20 1971"
+}
+
+Read about toDateString() for info on formatting a readable date. 
+
+*/
+// ***********SOLUTION**********
+
+function transformData(data) {
+   return data.map((data) => ({
+      fullname: `${data.name.first} ${data.name.last}`, //Simple way of accessing the object property using the dot operator.
+      birthday: new Date(data.dob.date).toDateString(), //toDateString is called upon the date object which returns the human readable date string "Fri Aug 20 1971" like so.
+   }));
+}
+
+console.log(transformData(userData));
