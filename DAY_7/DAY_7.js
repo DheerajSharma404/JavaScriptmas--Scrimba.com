31832e199f5bb8a6b7e85f41a048726dd0cc5f8c
Dheeraj Sharma <at60sharwan@gmail.com>
2022-12-07 12:28:30 +0530

    🎄JavaScriptmas 2022 Day-7 solution🎄--Created alternatCaps(a->A) fuction.

---
+++ DAY_7/DAY_7.js
@@ -0,0 +1,23 @@
+// ********JavaScriptmas_DAY-7_Challenge**********
+
+/* Alternating Caps 
+Write a function that takes in a string of letters
+and returns a sentence in which every other letter is capitalized.
+
+Example input: "I'm so happy it's Monday"
+Example output: "I'M So hApPy iT'S MoNdAy"
+*/
+
+// ***********SOLUTION**********
+
+function altCaps(str) {
+   //* Observe is that every character at even index is Upper case letter and at odd index is lower case.
+   return str
+      .split("") //* Converting the stirng into char array.
+      .map((char, index) =>
+         index % 2 == 0 ? char.toUpperCase() : char.toLowerCase()
+      ) //* Mapping the whole array and Capitalizing the alternate character.
+      .join(""); //* Joining them back into the Sentence.
+}
+
+console.log(altCaps("When you visit Portland you have to go to VooDoo Donuts"));
